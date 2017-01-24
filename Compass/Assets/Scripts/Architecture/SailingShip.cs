2945dbb3ea309d8efb5ecaf5bf68d812a74403fd
tsarachan <tes353@nyu.edu>
2017-01-24 11:11:19 -0500

    Ship movement accounts for wind

--- Compass/Assets/Scripts/Architecture/SailingShip.cs
+++ Compass/Assets/Scripts/Architecture/SailingShip.cs
@@ -18,22 +18,49 @@ public abstract class SailingShip : MonoBehaviour {
 	protected const string WIND_OBJ = "Wind";
 
 
+	/*
+	 * 
+	 * This variable helps determine how the ship is affected by wind.
+	 * 
+	 * It is added to the dot product of the ship's facing and the wind's direction.
+	 * 
+	 * If zero, the ship will be becalmed when perpendicular to the wind, and will move at normal speed
+	 * when traveling with the wind.
+	 * 
+	 * If 1, the ship will be becalmed when facing into the wind, will move at normal speed perpendicular to it,
+	 * and will move at double speed when traveling with it.
+	 * 
+	 */
+	public float dotAdjustment = 1.0f;
+
+
 	//internal variables
 	protected Rigidbody rb;
 
 
+	//initialize variables
 	protected virtual void Start(){
 		windScript = transform.root.Find(WEATHER_OBJ).GetComponent<Wind>();
 		rb = GetComponent<Rigidbody>();
 	}
 
+
+	//move ship in the direction it is facing
 	protected virtual void Update(){
 		rb.MovePosition(MoveForward());
 	}
 
 
+	/// <summary>
+	/// Determines where the ship should move, given its direction, speed, and the wind.
+	/// </summary>
+	/// <returns>The ship's new position.</returns>
 	protected virtual Vector3 MoveForward(){
-		return transform.position + (transform.forward * forwardSpeed * Time.deltaTime);
+		float windEffect = Vector3.Dot(transform.forward, windScript.GetWind()) + dotAdjustment;
+
+		return transform.position + 
+			   (transform.forward * forwardSpeed * Time.deltaTime) * 
+			   windEffect;
 	}
 
 
