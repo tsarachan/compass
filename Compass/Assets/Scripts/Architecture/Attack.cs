0cd3a7ea31137d44b5834737755ff7d2227a0a27
tsarachan <tes353@nyu.edu>
2017-02-03 10:43:01 -0500

    Architecture changes
    Created an abstract class for attacking, and a PlayerAttack class that uses inputs for attacks. Removed unnecessary LaunchProjectile class.

--- Compass/Assets/Scripts/Architecture/Attack.cs
+++ Compass/Assets/Scripts/Architecture/Attack.cs
@@ -1,41 +1,30 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public class BroadsideAttack : LaunchProjectile {
+public abstract class Attack : MonoBehaviour {
 
 	//variables relating to the cannonballs the player shoots
-	private const string CANNONBALL = "Cannonball";
+	protected const string CANNONBALL = "Cannonball";
 
 	//player controls
-	private KeyCode portFire = KeyCode.A;
-	private KeyCode starboardFire = KeyCode.S;
+	protected KeyCode portFire = KeyCode.A;
+	protected KeyCode starboardFire = KeyCode.S;
 
 
 	//internal variables
-	private const char PORT_ATTACK = 'P';
-	private const char STARBOARD_ATTACK = 'S';
+	protected const char PORT_ATTACK = 'P';
+	protected const char STARBOARD_ATTACK = 'S';
 
-
-	//initialize variables
-	protected override void Start(){
-		projectileName = CANNONBALL;
-	}
 	
 	//listen for inputs
-	private void Update () {
-		if (Input.GetKeyDown(portFire)){
-			Fire(PORT_ATTACK);
-		} else if (Input.GetKeyDown(starboardFire)){
-			Fire(STARBOARD_ATTACK);
-		}
-	}
+	protected abstract void Update();
 
 
 	/// <summary>
 	/// Fires a cannonball to port or starboard.
 	/// </summary>
 	/// <param name="dir">The direction of the attack, port or starboard.</param>
-	protected override void Fire(char dir){
+	protected void Fire(char dir){
 		GameObject myProjectile = ObjectPooling.ObjectPool.GetObj(CANNONBALL);
 
 		if (dir == PORT_ATTACK){
---
+++ Compass/Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerAttack : Attack {
+
+	protected override void Update(){
+		if (Input.GetKeyDown(portFire)){
+			Fire(PORT_ATTACK);
+		} else if (Input.GetKeyDown(starboardFire)){
+			Fire(STARBOARD_ATTACK);
+		}
+	}
+}
