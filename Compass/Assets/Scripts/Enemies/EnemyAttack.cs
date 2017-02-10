39b0246b28e1de5be1cc4dff7b89d58984cb18b1
tsarachan <tes353@nyu.edu>
2017-02-10 10:14:15 -0500

    Enemies don't attack each other
    Enemies will hold their fire if they would hit another enemy.

--- Compass/Assets/Scripts/Enemies/EnemyAttack.cs
+++ Compass/Assets/Scripts/Enemies/EnemyAttack.cs
@@ -8,6 +8,9 @@ public class EnemyAttack : Attack {
 	//----------Tunable variables----------
 	public float reloadDuration = 1.0f; //how often this enemy fires
 
+	//this is the size of the sphere enemies cast to see if they're in danger of hitting a friendly with a broadside
+	public float sphereRadius = 1.0f;
+
 
 	//----------Internal variables----------
 
@@ -23,10 +26,17 @@ public class EnemyAttack : Attack {
 	private const string CANNON_PORT = "Cannon port";
 
 
+	//these are used to find enemies, so that the enemy ships don't fire on each other
+	private int enemyLayer = 8;
+	private int enemyLayerMask;
+	private const string ENEMY_TAG = "Enemy";
+
+
 	//initialize variables
 	private void Start(){
 		player = GameObject.Find(PLAYER_OBJ).transform;
 		cannonPort = transform.Find(CANNON_PORT);
+		enemyLayerMask = 1 << enemyLayer; //bitwise math to create a layermask
 	}
 
 
@@ -44,8 +54,27 @@ public class EnemyAttack : Attack {
 	//determine whether the player is to the left or right, and fire accordingly
 	private void ChooseAttack(){
 		if (transform.InverseTransformPoint(player.position).x < 0.0f){
+			RaycastHit hitInfo;
+
+			//don't shoot if the enemy will hit a friendly
+			if (Physics.SphereCast(cannonPort.position, sphereRadius, -transform.right, out hitInfo, enemyLayerMask)){
+				if (hitInfo.transform.tag == ENEMY_TAG){
+					return;
+				}
+			}
+
 			Fire(PORT_ATTACK, cannonPort.position);
 		} else if (transform.InverseTransformPoint(player.position).x > 0.0f){
+			RaycastHit hitInfo;
+
+			//don't shoot if the enemy will hit a friendly
+			if (Physics.SphereCast(cannonPort.position, sphereRadius, transform.right, out hitInfo, enemyLayerMask)){
+				if (hitInfo.transform.tag == ENEMY_TAG){
+					return;
+				}
+			}
+
+
 			Fire(STARBOARD_ATTACK, cannonPort.position);
 		}
 	}
