fcded2e5907f33379790a27be4b32d6e7ff8b89c
tsarachan <tes353@nyu.edu>
2017-02-28 12:02:49 -0500

    Successful test task
    Successfully tested a trivial task that initializes, waits a prescribed number of seconds, and then cleans itself up.

---
+++ Compass/Assets/Scripts/Architecture/Tasks/TestTask.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTask : Task {
+
+	private float timer = 0.0f;
+	private float timeOver = 1.0f;
+
+	public TestTask(float timeOver){
+		this.timeOver = timeOver;
+	}
+
+	protected override void Init(){
+		Debug.Log("Init() called");
+	}
+
+	internal override void Update(){
+		Debug.Log("Update() called");
+
+		timer += Time.deltaTime;
+
+		if (timer >= timeOver){
+			SetStatus(TaskStatus.Succeeded);
+		}
+	}
+
+	protected override void Cleanup(){
+		Debug.Log("Cleanup() called");
+	}
+}
---
+++ Compass/Assets/Scripts/Enemies/BossBehavior.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBehavior : MonoBehaviour {
+
+	void Start () {
+		TestTask testTask = new TestTask(2.0f);
+		GameObject.Find("Game manager").GetComponent<TaskManager>().AddTask(testTask);
+	}
+}
