2c2e3abd3d0437b749e588f53af190b6a8931a3f
tsarachan <tes353@nyu.edu>
2017-03-14 16:24:11 -0400

    Building ChaseTask
    Creating task the boss can use to chase the player until the boss, or the player, is destroyed.

---
+++ Compass/Assets/Scripts/Architecture/Tasks/ChaseTask.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseTask : Task {
+
+	private SailingShip ship;
+	private Transform target;
+
+
+	//delegate that responds to TookDamageEvents; this is used to determine when the next task should begin
+	private TookDamageEvent.Handler damageFunc;
+
+
+	public ChaseTask(SailingShip ship, Transform target){
+		this.ship = ship;
+		this.target = target;
+	}
+
+
+	protected override void Init(){
+		if (ship == null || target == null){
+			SetStatus(TaskStatus.Aborted);
+		}
+
+		damageFunc = HandleDamage;
+		EventManager.Instance.Register<TookDamageEvent>(damageFunc);
+	}
+
+
+	internal override void Update(){
+
+	}
+
+
+	/// <summary>
+	/// Unregister for events.
+	/// </summary>
+	protected override void Cleanup(){
+		EventManager.Instance.Unregister<TookDamageEvent>(damageFunc);
+	}
+
+
+	/// <summary>
+	/// When a damage event is sent, see if it's this task's ship that was damaged. If so, and this task's ship was
+	/// destroyed, stop this task.
+	/// </summary>
+	/// <param name="e">The TookDamageEvent.</param>
+	private void HandleDamage(Event e){
+		TookDamageEvent damageEvent = e as TookDamageEvent;
+
+		if (damageEvent.ship.gameObject.name == ship.gameObject.name){
+			if (damageEvent.damagePercent <= 0.0f){
+				SetStatus(TaskStatus.Succeeded);
+			}
+		}
+	}
+}
